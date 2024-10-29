1bb935a283cc153c6c270186cc8efbb6035f5d97
Mike Mahon <mike@skipjackstudios.com>
2024-10-29 18:44:32 -0400

    Initial Commit

---
+++ GenmojiExporter/GenmojiExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenmojiExporterApp.swift
+//  GenmojiExporter
+//
+//  Created by Mike Mahon on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GenmojiExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
